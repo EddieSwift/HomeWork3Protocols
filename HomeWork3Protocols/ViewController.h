052dd7875d45b185be043879c14ad6b83ae7f4b9
Eddie <Eddie@galchenko.local>
2018-04-09 14:55:14 +0300

    Initial Commit

---
+++ HomeWork3Protocols/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HomeWork3Protocols
+//
+//  Created by Eddie on 09.04.2018.
+//  Copyright © 2018 Eddie Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
